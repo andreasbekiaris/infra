5a2cf00620bf9cde3fc428f59202c976fd555147
and13 <andreasmpekiaris2905@gamail.com>
2026-04-24 12:19:32 +0300

    generic fix  commit

--- InfraApi/obj/Debug/net8.0/InfraApi.AssemblyInfo.cs
+++ InfraApi/obj/Debug/net8.0/InfraApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1fe6a7d3ae9b27ab28d54012a59b02ab0016bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4ecabbd594dfac28187fcf2549edc21d442129")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
